de8652b3ef6e56cb9acf6a751f21df223692021c
faizzuns <faiz.sahupala29@gmail.com>
2018-12-05 11:49:37 +0700

    deleted popup

--- app/src/main/java/com/user/profileapplication/FriendsAdapter.java
+++ app/src/main/java/com/user/profileapplication/FriendsAdapter.java
@@ -56,9 +56,6 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
             @Override
             public void onClick(View view) {
                 listener.onFriendClicked(friend);
-                friends.remove(friend);
-                friend.delete();
-                notifyDataSetChanged();
             }
         });
     }
